74423dd685388c36f3c295d174604107ea866230
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2026-03-13 20:47:28 +0100

    fix: import expo-file-system from legacy path for SDK 55

--- lib/importDeck.ts
+++ lib/importDeck.ts
@@ -1,5 +1,5 @@
 import * as DocumentPicker from 'expo-document-picker';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { Platform } from 'react-native';
 
 export interface ImportedCard {
